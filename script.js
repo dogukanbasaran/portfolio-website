3b71d9dffdf3b5e75db0d9b24749ee1a8ef9b3fa
= <dogukanbasarann@hotmail.com>
2024-08-02 10:51:28 +0300

    project section is completed and project is fixed

--- script.js
+++ script.js
@@ -7,3 +7,19 @@ window.addEventListener('scroll', () => {
         header.classList.remove('scrolled');
     }
 })
+
+
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach((panel) => {
+    panel.addEventListener("click", () => {
+        removeActiveClasses();
+        panel.classList.add("active");
+    });
+});
+
+const removeActiveClasses = () => {
+    panels.forEach((panel) => {
+        panel.classList.remove("active");
+    })
+}
